398111d3c17256402debea40e7d03ae851a092ba
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-07-02 11:03:53 +0200

    rename to randomizer

---
+++ Source/Randomizer.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using ExampleMod;
+using NineSolsAPI;
+
+namespace Randomizer;
+
+[BepInDependency(NineSolsAPICore.PluginGUID)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Randomizer : BaseUnityPlugin {
+    private void Awake() {
+        Log.Init(Logger);
+        RCGLifeCycle.DontDestroyForever(gameObject);
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+
+    private void OnDestroy() {
+    }
+}
